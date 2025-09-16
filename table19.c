7149f9c22045d3e7b1eab36a7e2c522e780754fc
bhavay27 <27bhaweshdhawan@gmail.com>
2025-09-16 13:56:47 +0530

    Create table19.c

---
+++ table19.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i, k;
+    for(i = 0; i <= 10; i++) {
+        k = i * 19;
+        printf(" %d*19=%d\n", k);  // Added newline for readability
+    }
+
+    return 0;
